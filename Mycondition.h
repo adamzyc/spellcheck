4376435b30060e18d8c906f4534ee136475f922a
adamzyc <m18166546481@163.com>
2015-05-14 10:51:14 +0800

    mycondition

---
+++ Mycondition.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: Mycondition.h
+	> Author: 周雨晨
+	> Mail: 15917836@qq.com
+	> Created Time: Thu 14 May 2015 10:50:40 CST
+ ************************************************************************/
+
+#ifndef _MYCONDITION_H
+#define _MYCONDITION_H
+#endif
